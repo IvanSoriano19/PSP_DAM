0b5bf5c2423d1c4da4108ae5e77401777cdd52be
Ivan Soriano <ivansoriano1901@gmail.com>
2022-11-29 20:39:44 +0100

    prueba commit intelij

---
+++ Trabajos/bancoAbastos/src/main/java/BPA.java
@@ -0,0 +1,7 @@
+public class BPA {
+
+    public static void main(String[] args) {
+        Persona p = new Persona("Juan");
+        Cuenta c = new Cuenta(500);
+    }
+}
---
+++ Trabajos/bancoAbastos/src/main/java/Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+    private String nombre;
+
+    public Persona(String name){
+        nombre = name;
+    }
+
+    public int ingresarDinero(){
+
+        return 0;
+    }
+
+}
